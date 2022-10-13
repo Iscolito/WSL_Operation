a0a6c4fe210f4af44673f129318288df61da3502
Iscolito <108865458+Iscolito@users.noreply.github.com>
2022-10-13 09:29:10 +0800

    Add files via upload
    beta2 加入打开WSL服务和虚拟机平台选项

--- check.bat
+++ check.bat
@@ -12,6 +12,8 @@ echo 查看详细状态		ID=4
 echo 修改版本号(Ubuntu) 	ID=5
 echo 查看wsl中的文件		ID=6
 echo 变更用户权限		ID=7
+echo 开启WSL服务		ID=8
+echo 开启虚拟机平台		ID=9
 echo 更改虚拟硬盘地址	ID=reset
 echo 退出			ID=e
 echo ---------------------------------
@@ -26,8 +28,10 @@ if "%ID%"=="4"  goto cmd4
 if "%ID%"=="5"  goto cmd5
 if "%ID%"=="6"  goto cmd6
 if "%ID%"=="7"  goto cmd7
+if "%ID%"=="8"  goto cmd8
+if "%ID%"=="9"  goto cmd9
 if "%ID%"=="e"  goto con2
-if "%ID%"=="reset"  goto cmd8
+if "%ID%"=="reset"  goto cmdr
 goto back
 
 :cmd1
@@ -74,7 +78,7 @@ wsl --shutdown
 echo 已成功切换到%user_name%，并重启了wsl
 goto back
 
-:cmd8
+:cmdr
 set /p if_reset=**注意，此操作将重装Ubuntu，发生错误可能导致信息丢失，是否继续(y/n):
 if "%if_reset%"=="y"  goto reset
 if "%if_reset%"=="n"  goto back
@@ -88,6 +92,19 @@ echo 虚拟硬盘地址已切换到%target_dir%\ubuntu，您可以删除%target_
 echo 默认登录用户已设置为root
 goto back
 
+:cmd8
+set /p if_sudo=本操作需要管理员权限，请确定您是否打开管理员权限(y/n)
+if "%if_sudo%"=="n" goto back
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+echo 已成功开启WSL服务，请重启windows
+goto back
+
+:cmd9
+set /p if_sudo=本操作需要管理员权限，请确定您是否打开管理员权限(y/n)
+if "%if_sudo%"=="n" goto back
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+echo 已成功开启虚拟机平台，请重启windows
+goto back
 
 :back
 set /p choice=是否要返回菜单(y/n):
